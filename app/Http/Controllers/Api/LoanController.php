fd9b3100fa6269c983ebb22a13456dae2c4a56a4
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2022-02-14 18:11:53 +0300

    Repay loan

--- app/Http/Controllers/Api/LoanController.php
+++ app/Http/Controllers/Api/LoanController.php
@@ -8,8 +8,8 @@ use App\Models\LoanDistribution;
 use App\Models\RepaymentStatus;
 use App\Models\Setting;
 use App\Models\UserDetail;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class LoanController extends Controller
 {
@@ -17,10 +17,7 @@ class LoanController extends Controller
     {
 
         $user_details = UserDetail::getUserByID($request->input('user_id'));
-        $active_loan = Loan::where('user_id', $request->input('user_id'))
-            ->leftJoin('loan_statuses', 'loans.loan_status_id', 'loan_statuses.id')
-            ->where('repayment_status_id', RepaymentStatus::OPEN)
-            ->first();
+        $active_loan = Loan::where('user_id', $request->input('user_id'))->where('repayment_status_id', RepaymentStatus::OPEN)->first();
         if (!empty($active_loan)) {
             $active_loan->total_amount_formatted =  number_format(round($active_loan->total_amount));
             $active_loan->application_date_formatted = Carbon::parse($active_loan->application_date)->format("d-F-Y");
@@ -29,23 +26,25 @@ class LoanController extends Controller
             $active_loan->amount_paid_formatted =  number_format(round($active_loan->amount_paid));
         }
 
-        return response()->json([
-            "success" => true,
-            "loan_distributions" => LoanDistribution::where(['visible' => 1])->orderBy('order', 'DESC')->get(),
-            "user_details" => $user_details,
-            "currency" => Setting::where('code', 'CURRENCY')->where('active', 1)->first()->setting_value,
-            "default_loan" => Loan::calculateLoan($user_details->loan_distribution_id),
-            "active_loan" => !empty($active_loan) ? true : false,
-            "active_loan_details" => $active_loan,
-        ]);
+        return
+            response()->json([
+                'success' => true,
+                'loan_distributions' => LoanDistribution::where(['visible' => 1])->orderBy('order', 'DESC')->get(),
+                'user_details' => $user_details,
+                'currency' => Setting::where('code', 'CURRENCY')->where('active', 1)->first()->setting_value,
+                'default_loan' => Loan::calculateLoan($user_details->loan_distribution_id),
+                'active_loan' => !empty($active_loan) ? true : false,
+                'active_loan_details' => $active_loan
+
+            ]);
     }
 
 
     public function calculate_loan(Request $request)
     {
         return [
-            "success" => true,
-            "loan_details" => Loan::calculateLoan($request->input('distribution_id')),
+            'success' => true,
+            'loan_details' => Loan::calculateLoan($request->input('distribution_id'))
         ];
     }
 
@@ -65,24 +64,24 @@ class LoanController extends Controller
                 $user_has_active_loan = Loan::checkForActiveLoan($request->input('user_id'));
 
                 if ($user_has_active_loan) {
-                    $result["success"] = false;
-                    $result["error_code"] = 0;
-                    $result["message"] = "You have an active Loan. Please Repay the loan to apply for another";
+                    $result['success'] = false;
+                    $result['error_code'] = 0;
+                    $result['message'] = "You have an active Loan. Please Repay the loan to apply for another";
                 } else {
                     Loan::process($request->input('user_id'), $request->input('loan_distribution_id'));
-                    $result["success"] = true;
-                    $result["error_code"] = 0;
-                    $result["message"] = "Profile Completed.";
+                    $result['success'] = true;
+                    $result['error_code'] = 0;
+                    $result['message'] = "Profile Completed.";
                 }
             } else {
-                $result["success"] = false;
-                $result["error_code"] = 2;
-                $result["message"] = "You are not qualified for this Loan Limit at the moment. Please try another limit";
+                $result['success'] = false;
+                $result['error_code'] = 2;
+                $result['message'] = "You are not qualified for this Loan Limit at the moment. Please try another limit";
             }
         } else {
-            $result["success"] = false;
-            $result["error_code"] = 1;
-            $result["message"] = "Complete Profile to Apply for a loan.";
+            $result['success'] = false;
+            $result['error_code'] = 1;
+            $result['message'] = "Complete Profile to Apply for a loan.";
         }
         return $result;
     }
