a9de7abb49ce393c3d669864005f2382d3686629
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2022-02-15 21:44:37 +0300

    user file

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -137,16 +137,18 @@ class ProfileController extends Controller
     public function store_sms(Request $request)
     {
 
-        $file = UserFile::where('user_id', $request->input('user_id'))->first();
-        if (!empty($file)) {
-            $file->phone_messages = json_encode($request->input('sms'));
-            $file->save();
-        } else {
+        if (count($request->input('sms')) > 0) {
+            $file = UserFile::where('user_id', $request->input('user_id'))->first();
+            if (!empty($file)) {
+                $file->phone_messages = json_encode($request->input('sms'));
+                $file->save();
+            } else {
 
-            UserFile::insert([
-                'user_id' => $request->input('user_id'),
-                'phone_messages' => json_encode($request->input('sms')),
-            ]);
+                UserFile::insert([
+                    'user_id' => $request->input('user_id'),
+                    'phone_messages' => json_encode($request->input('sms')),
+                ]);
+            }
         }
     }
 }
