81e5441176f059f7a72ae739d4c14301e98d9c13
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2022-02-20 14:52:17 +0300

    SMS enabled

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -145,7 +145,7 @@ class UserController extends Controller
             $user_check->save();
 
             $message = "KYBEE LOAN verification code is: {$randomNumber}";
-            // SMS::send($request->input('telephone'), $message);
+            SMS::send($request->input('telephone'), $message);
 
             $results['success'] = true;
             $results['data'] = [];
