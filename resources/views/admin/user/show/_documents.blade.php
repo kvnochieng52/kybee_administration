c0b8bff47bed6fa01137e16932a29a51e91feb73
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2022-09-26 15:52:20 +0300

    Documents Tab

--- resources/views/admin/user/show/_documents.blade.php
+++ resources/views/admin/user/show/_documents.blade.php
@@ -1,14 +1,14 @@
 <div class="row">
     <div class="col-md-4">
-        <h3>ID FRONT</h3>
+        <h4>ID FRONT</h4>
         <a href="/{{$user_details->id_front}}"> <img src="/{{$user_details->id_front}}" style="width: 100%"></a>
     </div>
     <div class="col-md-4">
-        <h3>ID BACK</h3>
+        <h4>ID BACK</h4>
         <a href="/{{$user_details->id_back}}"> <img src="/{{$user_details->id_back}}" style="width: 100%"></a>
     </div>
     <div class="col-md-4">
-        <h3>SELFIE</h3>
+        <h4>SELFIE</h4>
         <a href="/{{$user_details->selfie}}"> <img src="/{{$user_details->selfie}}" style="width: 100%"></a>
     </div>
 </div>
