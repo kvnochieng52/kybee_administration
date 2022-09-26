e84d21e547d2dfc6961966c6fe2fe8fdcd20006b
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2022-09-26 15:44:17 +0300

    Documents Tab

--- resources/views/admin/user/show/_documents.blade.php
+++ resources/views/admin/user/show/_documents.blade.php
@@ -1,3 +1,5 @@
-<div class="col-md-4">1</div>
-<div class="col-md-4">2</div>
-<div class="col-md-4">3</div>
+<div class="row">
+    <div class="col-md-4">1</div>
+    <div class="col-md-4">2</div>
+    <div class="col-md-4">3</div>
+</div>
